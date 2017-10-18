6339352cb174dcfb1d0fd51c81df095fb14ff08e
JoNeil Starkie <Owner@joneils-air.home>
2017-10-18 22:45:11 +0100

    Added text when button is pressed

--- TestApplication/app/src/main/java/joneilstarkie/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/joneilstarkie/testapplication/MainActivity.java
@@ -2,6 +2,9 @@ package joneilstarkie.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.pressme_button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+                                          TextView passTimeView = (TextView) findViewById(R.id.question_text_view);
+                                          passTimeView.setText("Riding my Bike");
+                                      }
+                                  }
+        );
+
+
+
+
     }
 }
